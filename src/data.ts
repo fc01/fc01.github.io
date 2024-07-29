d0b616b494b9192f09e12cbe2f1739aaeef6175e
fc01 <910677221@qq.com>
2024-07-29 19:06:03 +0800

    `

---
+++ src/data.ts
@@ -0,0 +1,11 @@
+//@ts-ignore
+import mnist from 'mnist'
+type T = {
+    input: number[] //784
+    output: number[] //10
+}
+const data = mnist.set(8000, 2000) as {
+    training: T[]
+    test: T[]
+}
+console.log(data)
