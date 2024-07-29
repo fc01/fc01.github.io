464a8db44cb54ded44bfd4c4b5c050c300e28314
fc01 <910677221@qq.com>
2024-07-30 00:08:26 +0800

    ·

--- src/index.ts
+++ src/index.ts
@@ -31,9 +31,8 @@ const new_MLP = (arr: number[]): MLP => ({
 
 
 const 计算 = (mlp: MLP, data: number[]) => {
-    //
-    const first = mlp.layers[0].neurons
-    first.forEach((v, i) => v.output = data[i])
+    //    
+    mlp.layers[0].neurons.forEach((v, i) => v.output = data[i])
 
     for (let i = 1; i < mlp.layers.length; i++) {
         const prev = mlp.layers[i - 1].neurons
